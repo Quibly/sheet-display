79d728a90ac2e935af33d56be607c371ba461533
Adam Dutson <92229762+Quibly@users.noreply.github.com>
2026-05-06 15:43:51 -0600

    Update generate.js
    added Secret reference for SHEET_URL

--- generate.js
+++ generate.js
@@ -2,7 +2,11 @@ const fs = require("fs");
 const Papa = require("papaparse");
 
 // 🔧 PUT YOUR SHEET URL HERE
-const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTFUuAjoK9z114GjHURfSbMgwGcn2EplLALc09pwMshTnuNsnvp5T_vn2cKghjql9Fyo4XyiT6EGCkl/pub?gid=0&single=true&output=csv";
+const SHEET_URL = process.env.SHEET_URL;
+
+if (!SHEET_URL) {
+  throw new Error("Missing SHEET_URL environment variable");
+}
 
 async function fetchCSV() {
   const res = await fetch(SHEET_URL);
